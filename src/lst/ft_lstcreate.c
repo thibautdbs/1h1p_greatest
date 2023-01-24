c225bb555093b4d6ed07af4a0da870e239d7b1ff
tdubois <tdubois@student.42angouleme.fr>
2023-01-24 08:05:27 +0100

    fixed lstcreate

--- src/lst/ft_lstcreate.c
+++ src/lst/ft_lstcreate.c
@@ -6,22 +6,20 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 18:07:54 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/22 18:13:10 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/24 08:04:34 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "1h1p/ft_lst.h"
 
 #include <stddef.h>//NULL
-#include <stdlib.h>//malloc
-
-#include "1h1p/memory.h"
+#include <stdlib.h>//calloc
 
 t_lst	*ft_lstcreate(int content)
 {
 	t_lst	*node;
 
-	node = ft_calloc(1, sizeof(t_lst));
+	node = calloc(1, sizeof(t_lst));
 	if (node == NULL)
 		return (node);
 	node->content = content;
