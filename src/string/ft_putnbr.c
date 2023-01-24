e9c194f8ca161d62af21363769ba8caf37505379
tdubois <tdubois@student.42angouleme.fr>
2023-01-24 07:59:03 +0100

    wrote putnbr

--- src/string/ft_putnbr.c
+++ src/string/ft_putnbr.c
@@ -6,31 +6,35 @@
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 19:30:14 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/22 19:48:22 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/24 07:58:56 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "1h1p/ft_string.h"
 
+#include <unistd.h>//write
+
+static void	ft_putchar(char c);
+
 void	ft_putnbr(int n)
 {
-	char	nptr[8];
-	int		i;
+	int	leading_digits;
+	int	last_digit;
 
-	ft_bzero(nptr, 8);
-	i = 0;
+	leading_digits = n / 10;
+	last_digit = n % 10;
 	if (n < 0)
 	{
-		nptr[0] = '-';
-		n = -n;
-		i++;
+		ft_putchar('-');
+		leading_digits *= -1;
+		last_digit += -1;
 	}
-	while (i < 7 && n / 10 > 0)
-	{
-		nptr[i] = n % 10;
-		n = n / 10;
-		i++;
-	}
-	if (n / 10 > 0)
-		ft_putnbr(n);
+	if (leading_digits > 0)
+		ft_putnbr(leading_digits);
+	ft_putchar(last_digit + '0');
+}
+
+static void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
