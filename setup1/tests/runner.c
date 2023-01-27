f29e13c1033f4e545eed89216de4a8403e08509d
tdubois <tdubois@student.42angouleme.fr>
2023-01-27 09:04:08 +0100

    wrote tests for ft_lstpop_back.c

--- setup1/tests/runner.c
+++ setup1/tests/runner.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_runner.c                                      :+:      :+:    :+:   */
+/*   runner.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tdubois <tdubois@student.42angouleme.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:20:28 by tdubois           #+#    #+#             */
-/*   Updated: 2023/01/24 09:51:11 by tdubois          ###   ########.fr       */
+/*   Updated: 2023/01/27 08:54:32 by tdubois          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "greatest/greatest.h"
 
 extern SUITE(test_atoi);
+extern SUITE(test_lstpop_back);
 
 GREATEST_MAIN_DEFS();
 int	main(int argc, char **argv)
@@ -20,6 +21,7 @@ int	main(int argc, char **argv)
 	GREATEST_MAIN_BEGIN();
 
     RUN_SUITE(test_atoi);
+	RUN_SUITE(test_lstpop_back);
 
 	GREATEST_MAIN_END();
 }
